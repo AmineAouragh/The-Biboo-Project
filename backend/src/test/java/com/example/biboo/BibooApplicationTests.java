2f60f7a5eb94d4a4923cb6eb05ede9daf73efe42
Amine Aouragh <amine.aouragh.21@gmail.com>
2023-10-13 15:48:53 +0100

    set up the biboo project

---
+++ backend/src/test/java/com/example/biboo/BibooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.biboo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
