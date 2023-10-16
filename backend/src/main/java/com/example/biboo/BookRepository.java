99ae1b958df91c4fda0cf830d7f206ecf86aabe8
Amine Aouragh <amine.aouragh.21@gmail.com>
2023-10-16 13:31:48 +0100

    add custom queries to find all books, find by title and find by category

--- backend/src/main/java/com/example/biboo/BookRepository.java
+++ backend/src/main/java/com/example/biboo/BookRepository.java
@@ -1,15 +1,21 @@
 package com.example.biboo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+    @Query("SELECT b FROM Book b")
     List<Book> findAllBooks();
 
-    Book findBookByTitle(String bookTitle);
+    @Query("SELECT b FROM Book b WHERE b.bookTitle = :title")
+    Book findBookByTitle(String title);
 
-    Book findBookByStatus(String bookStatus);
+    @Query("SELECT b FROM Book b WHERE b.bookCategory = :bookCategory")
+    Book findBookByStatus(String bookCategory);
 
 }
