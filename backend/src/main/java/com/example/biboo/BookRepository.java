2e888a72ad91085a0e72bbbee98cc89cf16ea42e
Amine Aouragh <amine.aouragh.21@gmail.com>
2023-10-16 13:53:55 +0100

    fixed the findBookByTitle function return type from List to Book and the findBooksByCategory return type from Book to List

--- backend/src/main/java/com/example/biboo/BookRepository.java
+++ backend/src/main/java/com/example/biboo/BookRepository.java
@@ -2,6 +2,7 @@ package com.example.biboo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,9 +14,9 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findAllBooks();
 
     @Query("SELECT b FROM Book b WHERE b.bookTitle = :title")
-    Book findBookByTitle(String title);
+    Book findBookByTitle(@Param("title") String title);
 
-    @Query("SELECT b FROM Book b WHERE b.bookCategory = :bookCategory")
-    Book findBookByStatus(String bookCategory);
+    @Query("SELECT b FROM Book b WHERE b.bookCategory = :book_category")
+    List<Book> findBooksByCategory(@Param("book_category") String book_category);
 
 }
